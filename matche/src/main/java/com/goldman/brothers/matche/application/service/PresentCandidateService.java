60921565e03a7d824e6c1fa6df8b1c5b43eb40e0
YoshiyaNoda <ny0106.1425@gmail.com>
2022-10-07 00:43:00 +0900

    一つ目のusecaseのひな形作成

---
+++ matche/src/main/java/com/goldman/brothers/matche/application/service/PresentCandidateService.java
@@ -0,0 +1,10 @@
+package com.goldman.brothers.matche.application.service;
+
+/*
+まずあらかじめバッチジョブで算出したマッチングすべき候補のユーザをクライアントに渡す処理が必要
+*/
+class PresentCandidateService {
+    public List<CandidateDto> presentCandidateAtInitialPage(String userId) {
+        return ;
+    }
+}
