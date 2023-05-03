0203b68aeb30bd662904bc79b56517a9ddfe4735
Yoshiya Noda <ny1425.0106@gmail.com>
2023-05-03 19:56:50 +0900

    いろいろ

--- matching/spring-server/src/test/java/com/goldman/brothers/matche/ServerApplicationTests.java
+++ matching/spring-server/src/test/java/com/goldman/brothers/matche/ServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MatcheApplicationTests {
+class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
